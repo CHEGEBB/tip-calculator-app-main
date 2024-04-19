2e232d352721ca09f89c055c1da5c89dcca22eb5
CHEGEBB <chegephil24@gmail.com>
2024-04-19 13:20:59 +0300

    js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded',function (){
-    
+
+    const BillField =document.getElementById('bill')
     const FivePercent =document.getElementById('tip-five');
     const TenPercent =document.getElementById('tip-ten');
     const FifteenPercent=document.getElementById('tip-fifteen');
@@ -7,5 +8,6 @@ document.addEventListener('DOMContentLoaded',function (){
     const FiftyPercent =document.getElementById('tip-fifty');
     const CustomPercent =document.getElementById('percent-custom');
     const NumberOfPeople=document.getElementById('people');
+    
 
 });
