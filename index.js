d5867900c54b815cfe90cddfbd03bf70f273e757
CHEGEBB <chegephil24@gmail.com>
2024-04-19 13:18:09 +0300

    js

---
+++ index.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded',function (){
+    
+    const FivePercent =document.getElementById('tip-five');
+    const TenPercent =document.getElementById('tip-ten');
+    const FifteenPercent=document.getElementById('tip-fifteen');
+    const TwentyFivePercent =document.getElementById('tip-twenty-five');
+    const FiftyPercent =document.getElementById('tip-fifty');
+    const CustomPercent =document.getElementById('percent-custom');
+    const NumberOfPeople=document.getElementById('people');
+    console.log(NumberOfPeople);
+
+});
